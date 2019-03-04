3cf59aeb153434cd02012ccb290c39d737b7fc52
Isaac Elliott <isaace71295@gmail.com>
2019-03-04 18:23:20 +1000

    I wrote id and const :)

--- src/Lambda.hs
+++ src/Lambda.hs
@@ -1,23 +1,43 @@
 {-# language DeriveGeneric #-}
 {-# language FlexibleInstances, FlexibleContexts, MultiParamTypeClasses #-}
+{-# language StandaloneDeriving, QuantifiedConstraints, UndecidableInstances #-}
 module Lambda where
 
-import Data.Name.Class (Permutable(..), Nominal(..), Permutable1(..), Nominal1(..))
-import Data.Name.Substitution (Subst(..))
-import Data.Name.Tie (Tie)
-import Data.Name.Type (Name)
-import GHC.Generics (Generic)
+import Prelude hiding (id, (.), uncurry)
+
+import Control.Category (id, (.))
+
+import Data.Name
+import GHC.Generics (Generic, Generic1)
+
+import qualified Prelude
 
 data LamF a
   = Var !Name
   | App a a
   | Lam (Tie Name a)
-  deriving (Eq, Generic, Show)
+  deriving (Eq, Generic, Generic1, Show)
 instance Permutable a => Permutable (LamF a)
+instance Permutable1 LamF
 instance Nominal a => Nominal (LamF a)
+instance Nominal1 LamF
 instance (Subst a Name, Subst a a) => Subst a (LamF a)
 
+lamF :: Nom (Tie Name a) (LamF a)
+lamF = nom_ Lam
+
+appF :: Nom (a, a) (LamF a)
+appF = nom_ (Prelude.uncurry App)
+
+varF :: Nom Name (LamF a)
+varF = nom_ Var
+
 data Mu f = In { out :: f (Mu f) }
+
+deriving instance (forall x. Show x => Show (f x)) => Show (Mu f)
+
+mu :: Nom (f (Mu f)) (Mu f)
+mu = nom_ In
 
 instance Permutable1 f => Permutable (Mu f) where
   trans n1 n2 (In s) = In $ trans1 trans n1 n2 s
@@ -28,3 +48,14 @@ instance Nominal1 f => Nominal (Mu f) where
   supp (In s) = supp1 supp s
   supply (In s) = supply1 supply s
   equiv (In s) = equiv1 equiv s
+
+instance Subst1 e f => Subst e (Mu f) where
+  subst m p (In s) = In $ subst1 subst m p s
+
+id_ :: Nom () (Mu LamF)
+id_ = mu . lamF . leftAdjunct (mu . varF . pi2)
+
+const_ :: Nom () (Mu LamF)
+const_ =
+  mu . lamF .
+  leftAdjunct (mu . lamF . leftAdjunct (mu . varF . pi1) . pi2)
